3941924e92ad467ed690edac0f62ddedbd27341c
Christian Hofstaedtler <ch@zeha.at>
2006-01-15 06:11:32 +0000

    more sanity

--- Client/build-linux.sh
+++ Client/build-linux.sh
@@ -3,11 +3,11 @@ echo Qbe SAS Client Linux MakeRunner
 echo "\$Id$"
 echo Building common
 cd Common
-make -f Makefile.mono clean && make -f Makefile.mono
+make -f Makefile.mono clean all
 cd ..
 echo Building service
 cd Service
-make -f Makefile.mono
+make -f Makefile.mono clean all
 cd ..
 echo Building SetupLinux
 cd SetupLinux
