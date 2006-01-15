5a7cc7b2bff7f0ee42fe4360199889d9b3f7daef
Christian Hofstaedtler <ch@zeha.at>
2006-01-15 07:50:57 +0000

    update version

--- Client/Common/version.h
+++ Client/Common/version.h
@@ -2,14 +2,14 @@
 /// Master Header
 
 // if you need to only touch this file, change the following line:
-// asdf
+// asdf.
 
 /// UNICODE-Versionsnummer
-#define L_ILOGIN_VERSION L"2.24.08"
+#define L_ILOGIN_VERSION L"2.24.09"
 /// ASCII-Versionsnummer
-#define ILOGIN_VERSION "2.24.08"
+#define ILOGIN_VERSION "2.24.09"
 /// Versionsnummer fuer .NET Assemblies
-#define ILOGIN_ASSEMBLYVERSION "2.24.08.*"
+#define ILOGIN_ASSEMBLYVERSION "2.24.09.*"
 /// Versionskontrollsystem ID
 #define ILOGIN_CVSID "$Id$"
 
