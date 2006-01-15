ebcd40df8b6eaefec5b012f0b8c334cf71789353
Christian Hofstaedtler <ch@zeha.at>
2006-01-15 06:52:34 +0000

    better rc-script

--- Client/SetupLinux/qbe-install/install.sh
+++ Client/SetupLinux/qbe-install/install.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 
-INSTALLPATH=/opt/qbe
 VERSION=`cat version`
 
+echo "     ____  _               Qbe SAS Client (XPlat) $VERSION"
+echo "    / __ \| |              for Linux Installations"
+echo "   | |  | | |__   ___      "
+echo "   | |  | | '_ \ / _ \     Copyright 2001-2006 Christian Hofstaedtler"
+echo "   | |__| | |_) |  __/     "
+echo "    \___\_\_.__/ \___|     The \"2006 Happy New Year\"-Edition"
 echo ""
-echo "Qbe SAS Client $VERSION for Linux Installation"
-echo "The \"2005==WE ARE STILL ALIVE PREVIEW\"-Edition"
-echo "Copyright 2001-2005 Christian Hofstaedtler"
 echo ""
-
-rm -f .tempfile
-whiptail --title "Installation Path" --backtitle "Qbe SAS Client $VERSION" --inputbox "\nQbe SAS Client $VERSION for Linux\nCopyright 2001-2005 Christian Hofstaedtler\n\nEnter the path for the Qbe SAS Client installation:" 14 70 "$INSTALLPATH" 2> .tempfile
-INSTALLPATH=`cat .tempfile`
-rm -f .tempfile
-
-if [ "$?" == "1" ]; then
-	echo "Installation aborted."
-	exit 1
-fi
-
+read -p "Specify Destination Path: (/opt/qbe) " INSTALLPATH
 if [ "$INSTALLPATH" == "" ]; then
-	echo "Installation aborted."
-	exit 1
+	INSTALLPATH=/opt/qbe
 fi
 
 echo "Install location: $INSTALLPATH"
 echo ""
-echo -n "Continue (y/n)? "
-read answer
+read -p "Continue (y/n)? " answer
 if [ "$answer" != "y" ]; then
 	echo "Installation aborted."
 	exit 1
@@ -41,11 +31,13 @@ cp -a scripts/* ../QbeService.EXE $INSTALLPATH/
 echo ""
 echo ""
 echo "Hints:"
-echo "An rc.d script is in the file 'qbeservice'. It was written for"
-echo "  Debian Linux. For others, you may have to update it."
-echo "If you are using Gnome 2.x, you can use the authsave script to"
-echo "  save your username and password. authdel will delete this for"
-echo "  you. Then use qbelogon in your session config, it will start"
-echo "  the QbeService and log you on with your saved data."
+echo ""
+echo " * An rc.d script is provided in the file 'rc-script'. It was written for"
+echo "   Debian Linux. You may have to update it for other OS versions."
+echo ""
+echo " * If you are using Gnome 2.x, you can use the authsave script to save "
+echo "   your username and password. authdel will delete this for you. Then "
+echo "   use qbelogon in your session config: it will start QbeService and log "
+echo "   you on with your saved data."
 echo ""
 
